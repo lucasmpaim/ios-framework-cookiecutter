96c6c6a03acbd8288ad8cfdbfae9326898053018
Lucas Paim <lucas.paim@baritecnologia.com.br>
2019-12-10 07:58:32 -0300

    fix: compile in a temp folder

--- {{cookiecutter.framework_name}}/build.sh
+++ {{cookiecutter.framework_name}}/build.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 
 LOCKFILE=/tmp/.lock_{{cookiecutter.framework_name}}
+TMP_DIR=$(mktemp -d -t {{cookiecutter.framework_name}})
+
+rm -r "./{{cookiecutter.framework_name}}.xcframework"
 
 
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
@@ -12,7 +16,7 @@ trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
 echo $$ > ${LOCKFILE}
 
 xcodebuild archive -scheme {{cookiecutter.framework_name}} \
- -archivePath "./archives/iOS/{{cookiecutter.framework_name}}" \
+ -archivePath "${TMP_DIR}/iOS/{{cookiecutter.framework_name}}" \
   -sdk iphoneos \
    SKIP_INSTALL=NO \
    BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
@@ -20,7 +24,7 @@ xcodebuild archive -scheme {{cookiecutter.framework_name}} \
 
 
 xcodebuild archive -scheme {{cookiecutter.framework_name}} \
- -archivePath "./archives/simulator/{{cookiecutter.framework_name}}" \
+ -archivePath "${TMP_DIR}/simulator/{{cookiecutter.framework_name}}" \
   -sdk iphonesimulator \
    SKIP_INSTALL=NO \
    BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
@@ -28,8 +32,9 @@ xcodebuild archive -scheme {{cookiecutter.framework_name}} \
 
 
 xcodebuild -create-xcframework \
-	-framework "./archives/iOS/{{cookiecutter.framework_name}}.xcarchive/Products/Library/Frameworks/{{cookiecutter.framework_name}}.framework" \
-	-framework "./archives/simulator/{{cookiecutter.framework_name}}.xcarchive/Products/Library/Frameworks/{{cookiecutter.framework_name}}.framework" \
+	-framework "${TMP_DIR}/iOS/{{cookiecutter.framework_name}}.xcarchive/Products/Library/Frameworks/{{cookiecutter.framework_name}}.framework" \
+	-framework "${TMP_DIR}/simulator/{{cookiecutter.framework_name}}.xcarchive/Products/Library/Frameworks/{{cookiecutter.framework_name}}.framework" \
 	-output "./{{cookiecutter.framework_name}}.xcframework"
 
 rm -f ${LOCKFILE}
+rm -r ${TMP_DIR}
